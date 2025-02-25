201ecff8be4cb58f48793f93343b5a0c90d8c1de
dtwnkhung <quangnhat249@gmail.com>
2025-02-25 17:54:53 +0700

    feat: update UI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      // spacing: {
+      //   'word-wide': '0.2em',
+      // },
+      // fontFamily: {
+      //   inter: ["Inter", "sans-serif"], // Add Poppins as a custom font
+      // },
+    },
+  },
+  plugins: [],
+};
